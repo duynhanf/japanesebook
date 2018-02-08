9ac8833a4ce4af39d56ae50fd33ceac342430696
BuiDuyNhan <duynhanf@outlook.com>
2018-02-08 17:49:37 +0900

    remove item unused

--- JapaneseBook.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace JapaneseBook.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- JapaneseBook.WebApi/Global.asax.cs
+++ JapaneseBook.WebApi/Global.asax.cs
@@ -15,9 +15,8 @@ namespace JapaneseBook.WebApi
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
